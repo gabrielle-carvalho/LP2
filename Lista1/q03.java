18c562e25251f18ccae8f9a9f118c0f80f6183b7
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-04-01 20:13:17 -0300

    Create q03.java

---
+++ Lista1/q03.java
@@ -0,0 +1,6 @@
+// Em um sistema para o Detran, a carteira de motorista é representada por um número, CPF do proprietário, tipo (A, B ou AB), data de validade, data de expedição, 
+// pontos e situação (Valida, apreendida, vencida). Quando uma pessoa passa no exame do Detran, uma nova carteira é emitida com os dados do seu proprietário.
+//Neste momento o proprietário não possui nenhum ponto na carteira. A medida que o tempo passa, se o proprietário fizer alguma infração, ele recebe pontos 
+// que são acumulados em sua carteira. Caso ele ultrapasse 20 pontos sua carteira é apreendida. Esporadicamente, os pontos são zerados. 
+// Implemente em Java uma classe que represente uma carteira. Forneça métodos para criar uma carteira, acrescentar pontos, consultar o saldo de pontos, 
+// zerar a carteira, apreender a carteira, verificar se esta está válida.
