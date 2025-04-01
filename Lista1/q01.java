1aea77ed576a2a7dca2d87796efa8966625aad04
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-04-01 20:34:27 -0300

    Update q01.java

--- Lista1/q01.java
+++ Lista1/q01.java
@@ -1,9 +1,8 @@
-
 /**
- * Escreva uma descrição da classe Aluno aqui.
- * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
+Criar uma classe para representar aluno de uma faculdade. A classe possui os atributos matrícula, nome, ano de ingresso, curso, qtd de disciplinas matriculadas, situação do aluno (matriculado ou não matriculado). Esta classe deve possuir os seguintes métodos:
+- Método para criar um aluno com matricula, nome, ano de ingresso e curso.
+- Método para calcular o tempo (em anos) de permanência do aluno na faculdade. Para isso receber como argumento o ano atual e calcular há quantos anos o aluno já está na faculdade considerando seu ano de ingresso.
+- Método para calcular a mensalidade do aluno considerando R$ 150,00 por disciplina matriculada. 
  */
 public class Aluno
 {
