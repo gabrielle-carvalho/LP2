ba787f968a2aa6cb99176393160b5249f848cd1b
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-04-07 21:16:16 -0300

    corriji erro no return

--- Lista 2/slide.java
+++ Lista 2/slide.java
@@ -9,23 +9,23 @@ public class Medicamento
         this.precoCusto=precoCusto;
     }
     public Medicamento(String nome, float precoCusto){
-        this(nome, nome, precoCusto); // verificar se é assim pq tem o mesmo nome
+        this(nome, nome, precoCusto); //chama o construtor da mesma classe
     }
     
     public void setNome(String nome){
         this.nome=nome;
     }
     
-    public void setprincipioAtivo(String principioAtivo){
+    public void setPrincipioAtivo(String principioAtivo){
         this.principioAtivo=principioAtivo;
     }
     
-    public void setprecoCusto(float precoCusto){
+    public void setPrecoCusto(float precoCusto){
         this.precoCusto=precoCusto;
     }
     public float CalcPrecoVenda(float percentualLucro){
         float precoVenda;
-        return this.precoCusto*percentualLucro/100;
+        return this.precoCusto+(this.precoCusto*percentualLucro/100);
     }
     
     public float CalcPrecoVenda(){
