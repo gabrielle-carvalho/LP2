dd79ae1c32c1c45e07e7f3e8b73476dae34c23aa
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-04-07 22:05:57 -0300

    enunciado questao

---
+++ Lista 2/q02.java
@@ -0,0 +1,4 @@
+// 2. Um curso de uma faculdade é representado por um código, nome, duração (em semestres), valor total do curso e ano de criação do curso. Construa uma classe curso com os seguintes métodos:
+// · Criação de um curso com todos os dados necessários;
+// · Criação de um curso sem informar a duração. Neste caso o default são 8 semestres;
+// · Metódo para calcular o valor da mensalidade de um aluno, considerando que o valor é fíxo, ou seja, o valor total do curso pode ser pago em parcelas mensais.
