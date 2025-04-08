73cce45da7a99d57b2e98e81284331eca03b10c5
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-04-07 22:05:11 -0300

    fiz a errada

--- Lista 2/q01.java
+++ Lista 2/q01.java
@@ -1,27 +1,5 @@
-// Criar uma classe para representar aluno de uma faculdade. 
-// A classe possui os atributos matrícula, nome, ano de ingresso, curso, qtd de disciplinas matriculadas, situação do aluno (matriculado ou não matriculado).
-//Esta classe deve possuir os seguintes métodos:
-// - Método para criar um aluno com matricula, nome, ano de ingresso e curso.
-// - Método para calcular o tempo (em anos) de permanência do aluno na faculdade. 
-//Para isso receber como argumento o ano atual e calcular há quantos anos o aluno já está na faculdade considerando seu ano de ingresso.
-// - Método para calcular a mensalidade do aluno considerando R$ 150,00 por disciplina matriculada.
-
-public class Aluno{
-  private String nome, curso, situacao;
-  private int matricula, anoIngresso, qtdDisciplinas;
-  
-  public Aluno(int matricula, String nome, int anoIngresso, String curso){
-    this.matricula=matricula;
-    this.nome=nome;
-    this.anoIngresso=anoIngresso;
-    this.curso=curso;
-  }
-
-  int tempoPermanencia(int anoAtual){
-    return anoAtual-this.anoIngresso;
-  }
-  float mensalidade(int qtd){
-    return 150*qtd;
-  }
-  
-}
+// Uma loja de roupas possui um nome de fantasia, uma razão social, um número de cnpj, um dígito do cnpj, um valor de faturamento e uma área em m2. Construa uma classe loja conforme descrição acima e forneça os seguintes métodos:
+// · Método que permita criar uma loja informando o nome de fantasia, a razão social, o cnpj e o digito do cnpj;
+// · Método que permita criar uma loja informando o nome de fantasia, o cnpj e o digito do cnpj, o valor do faturamento e publico alvo. Neste caso considere que a razão social é igual ao nome de fantasia;
+// · Método que permita verificar se uma loja tem faturamento superior ao de outra loja
+// · Método que permita calcular o valor do aluguel de uma loja. Sabe-se que o aluguel custa R$50,00 por m2. 
