d2768939dac0ba04fe977ef9b74a85948bd49aa3
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-04-09 08:45:48 -0300

    classe para interagir com a q01
    feita inicialmete na lista 1, mas adaptada.
    fazer get % lucro

---
+++ Lista 2/Medicamento.java
@@ -0,0 +1,40 @@
+public class Medicamento
+{
+   private String nome, principioAtivo;
+   private float precoCusto;
+   private Fabricante produtor; //chama objeto da clase fabricante, tendo acesso a tudo
+   
+   public Medicamento(String nome, String principioAtivo, float precoCusto, Fabricante produtor){
+        this.nome=nome;
+        this.principioAtivo=principioAtivo;
+        this.precoCusto=precoCusto;
+        this.produtor= produtor;
+    }
+    
+    public Medicamento(String nome, float precoCusto){
+        this(nome, nome, precoCusto, null); //chama o construtor da mesma classe
+    }
+    
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    
+    public void setPrincipioAtivo(String principioAtivo){
+        this.principioAtivo=principioAtivo;
+    }
+    
+    public void setPrecoCusto(float precoCusto){
+        this.precoCusto=precoCusto;
+    }
+    
+    public void setProdutor(Fabricante produtor){
+        this.produtor=produtor;
+    }
+    
+    public float CalcPrecoVenda(){
+        float perc;
+        perc=produtor.getPercLucro();
+        return this.CalcPrecoVenda(perc);
+    }
+    
+}
