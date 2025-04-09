daaab1572884537ce92037456705459fffe5045d
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-04-09 09:00:17 -0300

    fala com medicamento

---
+++ Lista 2/Fabricante.java
@@ -0,0 +1,14 @@
+
+public class Fabricante
+{
+    private int cnpj;
+    private String nomeFantasia, endereco;
+    private float perLucro;
+    
+    public Fabricante(int cnpj, String nomeFantasia, String endereco, float perLucro){
+        this.cnpj=cnpj;
+        this.nomeFantasia=nomeFantasia;
+        this. endereco=endereco;
+        this.perLucro=perLucro;
+    }
+}
