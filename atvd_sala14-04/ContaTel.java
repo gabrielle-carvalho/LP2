128c3059c977443b4873227fcf2baf77260157a3
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-04-28 07:50:16 -0300

    Create ContaTel.java

---
+++ atvd_sala14-04/ContaTel.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class ContaTel{
+  private int mes, ano,contrato;
+  private ArrayList<Ligacao>ligacoes;
+  
+  public ContaTel(int mes, int ano, int contrato){
+    this.mes=mes;
+    this.ano=ano;
+    this.contrato=contrato;
+    this.ligacoes= new ArrayList<Ligacao>();
+  }
+  public void addLigacao(Ligacao lig){
+    ligacoes.add(lig);
+  }
+  public float totalConta(){
+    float total=0;
+    for(Ligacao lig: ligacoes){
+      total+=lig.valorLigacao();
+    }
+    return total;
+  }
+  
+}
