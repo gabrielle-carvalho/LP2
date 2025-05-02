da51509184abfcd5a249a39bd87c59ca30c74386
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-05-02 19:42:58 -0300

    Update q01.java

--- Prova 1 2013.2/q01.java
+++ Prova 1 2013.2/q01.java
@@ -1,1 +1,19 @@
+// A empresa Apple precisa construir um sistema para controlar os seus usuários e aplicativos. A empresa possui diversos usuários cadastrados e vende diversos aplicativos em sua loja virtual. Todo usuário possui um login, senha e data de ativação. Usuários adquirem aplicativos diversos. Todo aplicativo possui um nome, um objetivo, um tamanho (em MB) e um preço mensal (usuários pagam mensalmente pelos aplicativos que usam). Quando um usuário adquire um aplicativo ele é inserido na lista de usuários daquele aplicativo sendo liberado o seu acesso a este aplicativo.
+// Você foi contratado para desenvolver um sistema que atenda ao cenário acima.
+// 	Construa o modelo de classes UML que represente as classes que serão desenvolvidas para o sistema (1.0 ponto) e implemente em Java essas classes (só é preciso criar a classe e fazer a definição dos atributos e construtores. Considere que existem métodos get e set criados para todos os atributos de todas as classes). (1.0 ponto)
 
+
+
+// 	Construa um método que permita criar um aplicativo sem informar o preço. Neste caso o aplicativo é gratuito. (0.5 pontos)
+
+
+
+// 	Construa em Java os métodos necessários para saber se um aplicativo pode ser substituído por outro (um aplicativo poderá substituir outro se eles têm o mesmo objetivo e o preço é igual ou menor que o do outro aplicativo) (0.5 pontos).
+
+
+// 	Construa um método para saber quanto um usuário gasta mensalmente em aplicativos. (2.0)
+
+
+// 	Construa um método para a empresa avaliar, dado um usuário, se ele pode diminuir o valor que gasta mensalmente em aplicativos. Neste caso é preciso saber se ele pode substituir algum dos aplicativos que ele usa atualmente por um mais barato (2.5 pontos).
+
+// 	Construa em Java os métodos necessários para informar qual o aplicativo que tem mais usuários (3.0 pontos) (2.5 pontos).
