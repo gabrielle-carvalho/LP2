cea06d1beb7c4190da9ad90e5d906f4da96e6c51
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-05-02 17:54:59 -0300

    metodos feitos, q completa

--- Lista /questao03/Faculdade.java
+++ Lista /questao03/Faculdade.java
@@ -1,13 +1,7 @@
-// · Adicionar um Curso (passado como parâmetro) à faculdade. Entretanto, não é possível adicionar um curso já existindo um outro curso com o mesmo nome na Faculdade.
-
-// · Remover um Curso dado um nome, passado como parâmetro. Se não houver nenhum curso com o nome fornecido, uma mensagem deve ser impressa na tela informando o fato;
-
-// · Retornar uma nova Faculdade (uma nova instância da classe Faculdade) que contenha como cursos, os cursos que foram criados no máximo 5 anos depois da Faculdade. Este método deve receber como parâmetro o nome e o ano de criação da nova faculdade.
-
 import java.util.ArrayList;
 public class Faculdade{
   private String nome;
-  private int anoCriacao
+  private int anoCriacao;
   private ArrayList<Curso> course = new ArrayList<Curso>();
 
   public Faculdade(String nome, int anoCriacao){
@@ -16,7 +10,38 @@ public class Faculdade{
   }
 
   public void addCurso(Curso curs){
-    this.course.add(curs);
+  for (course c : curs) {
+    if(c.getNome().equalsIgnoreCase(curs.getNome()){
+      System.out.println("Curso já existe" + cus.getNome());
+      return;
+    }
+  }
+      this.course.add(curs);  
+  }
+  
+  public void removCurso(String nome){
+    boolean removido =false;
+    for(int i=0;i<course.size();i++){
+      if(couse.get(i).getNome().equalsIgnoreCase(nome)){
+        course.remove(i);
+        System.out.println("curso removido");
+        removido=true;
+        break;
+      }
+    }
+    if(removido==true){
+      System.out.println("curso removido");
+    }
+  }
+
+  public Faculdade filtrarCursos (String nomeNewFacul, int anoNewFacul){
+    Faculdade nova = new Faculdade(nomeNewFacul, anoNewFacul);
+    for(Curso c: course){
+      if(c.getAnoCriacao()<=this.anoCriacao+5){
+        nova.addCurso(c);
+      }
+    }
+    return nova;
   }
   
   public String getNome(){
