a1a815ca28419ab2013a6042dde0c80e2be0cae4
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-05-12 08:42:50 -0300

    feat: criando classe pai "sala"

---
+++ Aula5/exercicio2/Sala.java
@@ -0,0 +1,24 @@
+public class Sala{
+  protect int numeroSala, capacidade;
+  public Sala(int numeroSala, int capacidade){
+    this.numeroSala = numeroSala;
+    this.capacidade = capacidade;
+  }
+  
+  public String getNumeroSala() {
+        return numeroSala;
+    }
+
+  public void setNumeroSala(String numeroSala) {
+      this.numeroSala = numeroSala;
+  }
+  
+  public String getCapacidade() {
+        return capacidade;
+    }
+
+  public void setCapacidade(String capacidade) {
+      this.capacidade = capacidade;
+  }
+
+}
