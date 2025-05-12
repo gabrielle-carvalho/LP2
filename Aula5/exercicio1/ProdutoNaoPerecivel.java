558e97111a83dabd0f7785c90838f490152637e0
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-05-12 17:34:39 -0300

    fix: protected arrumado

--- Aula5/exercicio1/ProdutoNaoPerecivel.java
+++ Aula5/exercicio1/ProdutoNaoPerecivel.java
@@ -1,12 +1,15 @@
 public class ProdutoNaoPerecivel extends Produto{
-  protect String substancia;
+  
+  protected String substancia;
   public ProdutoNaoPerecivel(String codigo, String nome, float precoCusto, String substancia){
     super(codigo, nome, precoCusto);
     this(substancia);
   }
+  
   public String getSubstancia() {
         return substancia;
-    }
+  }
+  
   public void setSubstancia(String substancia) {
       this.substancia = substancia;
   }
