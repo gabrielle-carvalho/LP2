f67a87d3334ae2377f0c80467a2dbbca6827c01a
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-05-26 08:31:04 -0300

    feat: arraylist de passagens

--- Lista 4/Automovel.java
+++ Lista 4/Automovel.java
@@ -1,9 +1,15 @@
 public class Automovel{
     protected String placa, anoFabricacao;
+    protected ArrayList<Passagem> minhasPassagens;
 
     public Automovel (String placa, String anoFabricacao){
         this.placa=placa;
         this.anoFabricacao=anoFabricacao;
+        this.minhasPassagens = new ArrayList<Passagem>;
+    }
+
+    public void addPassagem (Passagem pass){
+        this.minhasPassagens.add(pass);
     }
 
     public void setPlaca(String placa){
